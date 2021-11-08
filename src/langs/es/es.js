5ec3a005686b730e4a704af3650782ee40a881ce
Adria Lopez <adria@prealfa.com>
2021-11-08 13:42:09 +0100

    Implemented Spanish translation

---
+++ src/langs/es/es.js
@@ -0,0 +1,17 @@
+//Add strings
+gdprCookieNoticeLocales.es = {
+  description: 'Usamos cookies para ofrecerte una mejor experiencia de navegación, personalizar el contenido y la publicidad, proporcionar funcionalidades de redes sociales y analizar el tráfico. Lee sobre cómo usamos las cookies y cómo puedes controlarlas haciendo click en Preferencias de cookies. Si continuas usando este sitio estás dando consentimiento a nuestro uso de cookies.',
+  settings: 'Preferencias de cookie',
+  accept: 'Aceptar cookies',
+  statement: 'Nuestra política de cookies',
+  save: 'Guardar preferencias',
+  always_on: 'Siempre activas',
+  cookie_essential_title: 'Cookies esenciales',
+  cookie_essential_desc: 'Cookies necesarias para hacer el sitio funcional, habilitando funcionalidades como la navegación o el acceso a áreas seguras del sitio web. El sitio web no puede funcionar correctamente sin estas cookies.',
+  cookie_performance_title: 'Cookies de personalización',
+  cookie_performance_desc: 'Estas cookies son necesarias para mejorar el rendimiento y la funcionalidad del sitio web pero no son esenciales para su uso. Por ejemplo, almacenan tu idioma preferido o la región en la que te encuentras.',
+  cookie_analytics_title: 'Cookies de analíticas',
+  cookie_analytics_desc: 'Usamos cookies de analíticas para medir cómo los usuarios interaccionan con el contenido del sitio, lo que nos permite personalizar el sitio web para mejorar tu experiencia.',
+  cookie_marketing_title: 'Cookies de marketing',
+  cookie_marketing_desc: 'Estas cookies se usan para hacer los anuncios más relevantes para tus intereses. La intención es mostrarte anuncios que son relevantes e interesantes para cada usuario individual y por tanto más valiosos para los autores y anunciantes.'
+}
